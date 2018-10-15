24f5287ec351055e93561db23a518a7ee3cd188a
tsoo6464 <tsoo6464@gmail.com>
2018-10-15 12:49:01 +0800

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Nan on 2018/10/15.
+//  Copyright © 2018 nan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
